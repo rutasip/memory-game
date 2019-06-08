2c6054e1e9d42e08661692747051040d5b924077
Ruta Sipkauskaite <ruta.sipkauskaite@gmail.com>
2019-06-08 20:06:10 +0300

    Updates

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ class MixOrMatch {
 function ready() {
   let overlays = Array.from(document.getElementsByClassName("overlay-text"));
   let cards = Array.from(document.getElementsByClassName("card"));
-  let game = new MixOrMatch(350, cards);
+  let game = new MixOrMatch(300, cards);
 
   overlays.forEach(overlay => {
     overlay.addEventListener("click", () => {
